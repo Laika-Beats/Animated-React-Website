df059d79e8f3ef6fa5176a49dd9faed6de299c51
Laika-Beats <laikabeatsdenver@gmail.com>
2021-02-19 13:45:58 -0700

    staggering animations

--- src/components/AboutSection.js
+++ src/components/AboutSection.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { About, Description, Image, Hide } from "../styles";
 import { motion } from "framer-motion";
+import { titleAnimation, fade, photoAnimation } from "../animation";
 import aboutImage from "../img/home1.png";
 
 const AboutSection = () => {
@@ -9,26 +10,30 @@ const AboutSection = () => {
       <Description>
         <motion.div>
           <Hide>
-            <motion.h2 v>We work to make</motion.h2>
+            <motion.h2 variants={titleAnimation}>We work to make</motion.h2>
           </Hide>
           <Hide>
-            <motion.h2>
+            <motion.h2 variants={titleAnimation}>
               your <span>dreams</span> come
             </motion.h2>
           </Hide>
           <Hide>
-            <motion.h2>true.</motion.h2>
+            <motion.h2 variants={titleAnimation}>true.</motion.h2>
           </Hide>
         </motion.div>
-        <p>
+        <motion.p variants={fade}>
           Contact us with any photo or video ideas. We have a team on standby to
           turn your dreams in to reality.
-        </p>
-        <button>Contact Us</button>
+        </motion.p>
+        <motion.button variants={fade}>Contact Us</motion.button>
       </Description>
 
       <Image>
-        <img src={aboutImage} alt="Young man sitting in nature" />
+        <motion.img
+          variants={photoAnimation}
+          src={aboutImage}
+          alt="Young man sitting in nature"
+        />
       </Image>
     </About>
   );
